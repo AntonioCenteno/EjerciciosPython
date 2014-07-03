d731c6eceed49faf5f4a6f2b317aa92dc9c96a60
dma-source <dmasourcegye@gmail.com>
2014-07-03 00:28:10 -0500

    Update calse.py

--- calse.py
+++ calse.py
@@ -4,21 +4,22 @@ op=""
 
 #este script enseña cómo se crea una clase, fue sacado en base a un ejemplo de codigofacilito
 class Postre:
-def __init__(self,nombre):
-self.nombre=nombre
-print nombre
+  def __init__(self,nombre):
+      self.nombre=nombre
+      print nombre
 
-def Ingrediente(self,nombre,indice):
-self.nombre=nombre
-self.indice=indice
-print nombre+": ", indice
+  def Ingrediente(self,nombre,indice):
+      self.nombre=nombre
+      self.indice=indice
+      print nombre+": ", indice
 
 
 while op!="n":
-system("cls")
-i+=1
-nom=raw_input("Ingrese el nombre del postre: ")
-nom2=raw_input("ingrese el ingrediente: ")
-postre=Postre(nom)
-postre.Ingrediente(nom2,i)
-op=raw_input("Desea Continuar (s/n): ")
+    system("cls")
+    i+=1
+    nom=raw_input("Ingrese el nombre del postre: ")
+    nom2=raw_input("ingrese el ingrediente: ")
+    postre=Postre(nom)
+    postre.Ingrediente(nom2,i)
+    op=raw_input("Desea Continuar (s/n): ")
+  
